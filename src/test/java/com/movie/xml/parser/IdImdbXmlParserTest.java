a2bf66cfc6a4f67bc4a4562f0ec03d9571361d91
asmirnova <asmirnova@playtika.com.ua>
2013-03-21 01:27:52 +0200

    Add ignore for unimplemented test

--- src/test/java/com/movie/xml/parser/IdImdbXmlParserTest.java
+++ src/test/java/com/movie/xml/parser/IdImdbXmlParserTest.java
@@ -1,13 +1,12 @@
 package com.movie.xml.parser;
 
-import com.movie.xml.parser.ImdbXmlParser;
-import com.movie.xml.parser.IdImdbXmlParser;
 import com.movie.jaxb.document.IdImdbDocument;
 import com.movie.xml.utils.ResponseUtils;
 import org.junit.Test;
 
 import java.util.List;
 import static org.junit.Assert.*;
+import org.junit.Ignore;
 
 public class IdImdbXmlParserTest {
 
@@ -19,6 +18,12 @@ public class IdImdbXmlParserTest {
         List<IdImdbDocument> actualDocsList = parser.parse(ResponseUtils.convertToDocument(xmlResponse));
         assertTrue(actualDocsList.size() == 1);
         assertEquals(expectedDoc, actualDocsList.get(0));
+    }
+    
+    @Ignore(value = "Not implemented yet")
+    @Test
+    public void parseMalformedDocument() {
+        
     }
 
 }
