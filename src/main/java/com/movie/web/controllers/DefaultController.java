79b3e6fa75d16e1e9904f3b63b4ec3238ec8f55b
AlorenNet <AlorenNet@AlorenNet-PC>
2013-05-12 23:00:28 +0300

    Replace sysout messages by loggong

--- src/main/java/com/movie/web/controllers/DefaultController.java
+++ src/main/java/com/movie/web/controllers/DefaultController.java
@@ -1,6 +1,8 @@
 package com.movie.web.controllers;
 
 import javax.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,14 +14,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class DefaultController {
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     @RequestMapping("/default")
     public String defaultAfterLogin(HttpServletRequest request) {
-        System.out.println("Logged in user " + SecurityContextHolder.getContext().getAuthentication());
+        logger.info("Logged in user " + SecurityContextHolder.getContext().getAuthentication());
         if (request.isUserInRole("ROLE_ADMIN")) {
-            System.out.println("Redirecting admin to admin page");
+            logger.info("Redirecting admin to admin page");
             return "redirect:/admin";
         }
-        System.out.println("Redirecting to /");
+        logger.info("Redirecting to /");
         return "redirect:/";
     }
 }
